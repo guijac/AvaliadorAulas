b0714d64fe5c08cf7a46cd5467bf453211f0e367
Guilherme <guilherme.cruz@usp.br>
2019-04-16 20:31:21 -0300

    Inicializando projeto

---
+++ src/main/java/br/pucsp/AvaliadorAulasAppApplication.java
@@ -0,0 +1,13 @@
+package br.pucsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorAulasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorAulasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/pucsp/AvaliadorAulasAppApplicationTests.java
@@ -0,0 +1,16 @@
+package br.pucsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvaliadorAulasAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
